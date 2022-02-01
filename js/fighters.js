406bac0fbb17a267c2a6746111974c55f5968d34
astrakore <teresa.carrasco.sauri@gmail.com>
2022-02-01 19:07:55 +0100

    Cambiados div a img en personajesAbajo

--- js/fighters.js
+++ js/fighters.js
@@ -1,11 +1,16 @@
 class Luchador {
 
-    constructor(nombre,vida,fuerza,defensa,velocidad) {
+    constructor(nombre,vida,fuerza,defensa,velocidad,disabled) {
         this.nombre = nombre;
         this.vida = vida;
         this.fuerza = fuerza;
         this.defensa = defensa;
         this.velocidad = velocidad;
+        this.disabled = disabled;
+    }
+
+    pegar() {
+        return this.fuerza;
     }
 
     cancelarGolpe() {
@@ -15,21 +20,34 @@ class Luchador {
 
 // Instanciando luchadores
 
-let luchador1 = new Luchador("Queen",1000,250,250,250);
-let luchador2 = new Luchador("Susie",1150,300,150,150);
-let luchador3 = new Luchador("Annoying Dog",Math.random(1000,5001),Math.random(100, 601),Math.random(200,601),Math.random(99, 401));
-let luchador4 = new Luchador("Ralsei",1100,200,150,300);
-let luchador5 = new Luchador("Temmie",Math.random(1000,1701),Math.random(100,601),200,300);
-let luchador6 = new Luchador("Sans",1666,400,300,100);
-let luchador7 = new Luchador("Noelle",1100,250,250,350);
-let luchador8 = new Luchador("Berdly",1250,200,200,350);
+let fighter1 = new Luchador("Queen",1000,250,250,250,"false");
+let fighter2 = new Luchador("Susie",1150,300,150,150,"false");
+let fighter3 = new Luchador("Annoying Dog",Math.random(1000,5001),Math.random(100, 601),Math.random(200,601),Math.random(99, 401),"false");
+let fighter4 = new Luchador("Ralsei",1100,200,150,300,"false");
+let fighter5 = new Luchador("Temmie",Math.random(1000,1701),Math.random(100,601),200,300,"false");
+let fighter6 = new Luchador("Sans",1666,400,300,100,"false");
+let fighter7 = new Luchador("Noelle",1100,250,250,350,"false");
+let fighter8 = new Luchador("Berdly",1250,200,200,350,"false");
+
+// Traductor
+
+let allFighters = {
+    1 : fighter1,
+    2 : fighter2,
+    3 : fighter3,
+    4 : fighter4,
+    5 : fighter5,
+    6 : fighter6,
+    7 : fighter7,
+    8 : fighter8
+}
 
 // Generando variables básicas del entorno
 
-let allFighters = [luchador1,luchador2,luchador3,luchador4,luchador5,luchador6,luchador7,luchador8];
 
-let jugador1 = [];
 
-let jugador2 = [];
+let jugador1 = "";
+
+let jugador2 = "";
 
 let ganador = "";
--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 // Funciones
 
+// ---------------------------------------- NUEVO JUEGO ---------------------------------------- 
+
 const cleanGame = () => {
-    jugador1 = [];
-    jugador2 = [];
+    jugador1 = "";
+    jugador2 = "";
     ganador = "";
 }
+
+// ---------------------------------------- CAMBIO DE PANTALLA ---------------------------------------- 
 
 const cambiaPantalla = (cambio) => {
     let pantallaDeseada ="screen" + cambio;
@@ -22,167 +26,269 @@ const cambiaPantalla = (cambio) => {
 
 // ---------------------------------------- SELECCIÓN DE PERSONAJES JUGADOR 1 ---------------------------------------- 
 
-const luchador1player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador1 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("queen").style.backgroundImage = "url('img/seleccionQueenBYN.png')";
-}
+//     document.getElementById("queen").style.backgroundImage = "url('img/seleccionQueenBYN.png')";
+// }
 
-const luchador2player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador2 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("susie").style.backgroundImage = "url('img/seleccionSusieBYN.png')";
-}
+//     document.getElementById("susie").style.backgroundImage = "url('img/seleccionSusieBYN.png')";
+// }
 
-const luchador3player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador3 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("annoyingDog").style.backgroundImage = "url('img/seleccionAnnoyingDogBYN.png')";
-}
+//     document.getElementById("annoyingDog").style.backgroundImage = "url('img/seleccionAnnoyingDogBYN.png')";
+// }
 
-const luchador4player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador4 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("ralsei").style.backgroundImage = "url('img/seleccionRalseiBYN.png')";
-}
+//     document.getElementById("ralsei").style.backgroundImage = "url('img/seleccionRalseiBYN.png')";
+// }
 
-const luchador5player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador5 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("temmie").style.backgroundImage = "url('img/seleccionTemmieBYN.png')";
-}
+//     document.getElementById("temmie").style.backgroundImage = "url('img/seleccionTemmieBYN.png')";
+// }
 
-const luchador6player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador6 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("sans").style.backgroundImage = "url('img/seleccionSansBYN.png')";
-}
+//     document.getElementById("sans").style.backgroundImage = "url('img/seleccionSansBYN.png')";
+// }
 
-const luchador7player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador7 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("noelle").style.backgroundImage = "url('img/seleccionNoelleBYN.png')";
-}
+//     document.getElementById("noelle").style.backgroundImage = "url('img/seleccionNoelleBYN.png')";
+// }
 
-const luchador8player1Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player1luchador8 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("berdly").style.backgroundImage = "url('img/seleccionBerdlyBYN.png')";
-}
+//     document.getElementById("berdly").style.backgroundImage = "url('img/seleccionBerdlyBYN.png')";
+// }
 
-// ---------------------------------------- SELECCIÓN DE PERSONAJES JUGADOR 2 ---------------------------------------- 
+// // ---------------------------------------- SELECCIÓN DE PERSONAJES JUGADOR 2 ---------------------------------------- 
 
-const luchador1player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador1 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("queen2").style.backgroundImage = "url('img/seleccionQueenBYN.png')";
-}
+//     document.getElementById("queen2").style.backgroundImage = "url('img/seleccionQueenBYN.png')";
+// }
 
-const luchador2player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador2 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("susie2").style.backgroundImage = "url('img/seleccionSusieBYN.png')";
-}
+//     document.getElementById("susie2").style.backgroundImage = "url('img/seleccionSusieBYN.png')";
+// }
 
-const luchador3player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador3 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("annoyingDog2").style.backgroundImage = "url('img/seleccionAnnoyingDogBYN.png')";
-}
+//     document.getElementById("annoyingDog2").style.backgroundImage = "url('img/seleccionAnnoyingDogBYN.png')";
+// }
 
-const luchador4player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador4 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("ralsei2").style.backgroundImage = "url('img/seleccionRalseiBYN.png')";
-}
+//     document.getElementById("ralsei2").style.backgroundImage = "url('img/seleccionRalseiBYN.png')";
+// }
 
-const luchador5player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador5 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("temmie2").style.backgroundImage = "url('img/seleccionTemmieBYN.png')";
-}
+//     document.getElementById("temmie2").style.backgroundImage = "url('img/seleccionTemmieBYN.png')";
+// }
 
-const luchador6player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador6 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("sans2").style.backgroundImage = "url('img/seleccionSansBYN.png')";
-}
+//     document.getElementById("sans2").style.backgroundImage = "url('img/seleccionSansBYN.png')";
+// }
 
-const luchador7player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador7 = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("noelle2").style.backgroundImage = "url('img/seleccionNoelleBYN.png')";
-}
+//     document.getElementById("noelle2").style.backgroundImage = "url('img/seleccionNoelleBYN.png')";
+// }
 
-const luchador8player2Selec = (numLuch) => {
-    let luchadorDeseado = "luchador" + numLuch;
+// const player2luchador8Selec = (numLuch) => {
+//     let luchadorDeseado = "luchador" + numLuch;
     
-    let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
+//     let arrayLuchadores = ["luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7","luchador8"];
 
-    arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
+//     arrayLuchadores = arrayLuchadores.filter(numLuch => !luchadorDeseado.includes(numLuch));
 
-    document.getElementById("berdly2").style.backgroundImage = "url('img/seleccionBerdlyBYN.png')";
+//     document.getElementById("berdly2").style.backgroundImage = "url('img/seleccionBerdlyBYN.png')";
+// }
+
+const selectFighter = (nLuchador) => {
+
+    if (jugador1 == "") {
+        jugador1 = allFighters[nLuchador];
+
+        let luchador1 = document.getElementById(nLuchador);
+        let datosLuchador1 = document.getElementById("data"+ 1);
+
+        luchador1.disabled = true;
+
+        luchador1.classList.add("luchador"[nLuchador] + "Seleccion");
+        datosLuchador1.innerHTML = `${jugador1.nombre}`;
+    } else if (jugador2 == "") {
+        jugador2 = allFighters[nLuchador];
+
+        let luchador2 = document.getElementById(nLuchador);
+        let datosLuchador2 = document.getElementById("data"+ 2);
+
+        luchador2.disabled = true;
+
+        luchador2.classList.add("luchador"[nLuchador] + "Seleccion");
+        datosLuchador2.innerHTML = `${jugador2.nombre}`;
+    }
+
+    setTimeout(()=>{
+        cambiaPantalla(3);
+    },2500);
 }
 
 // Algoritmo
 
+switch (jugador1) {
+    case fighter1:
+        
+    break;
+
+    case fighter2:
+    
+    break;
+
+    case fighter3:
+    
+    break;
+
+    case fighter4:
+    
+    break;
+    
+    case fighter5:
+    
+    break;
+
+    case fighter6:
+    
+    break;
+
+    case fighter7:
+    
+    break;
+
+    case fighter8:
+    
+    break;
+
+    default:
+        break;
+}
+
+switch (jugador1) {
+    case fighter1:
+        
+    break;
+
+    case fighter2:
+    
+    break;
+
+    case fighter3:
+    
+    break;
+
+    case fighter4:
+    
+    break;
+    
+    case fighter5:
+    
+    break;
+
+    case fighter6:
+    
+    break;
+
+    case fighter7:
+    
+    break;
+
+    case fighter8:
+    
+    break;
+
+    default:
+        break;
+}
