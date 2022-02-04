7cdb6bea5f198a60658452fd812e8953a98bd68c
astrakore <teresa.carrasco.sauri@gmail.com>
2022-02-04 15:54:27 +0100

    Limpiado código

--- js/main.js
+++ js/main.js
@@ -26,49 +26,6 @@ const cambiaPantalla = (cambio) => {
 
 // ---------------------------------------- SELECCIONAR LUCHADOR ---------------------------------------- 
 
-// const selectFighter = (nLuchador) => {
-
-//     if (jugador1 == "") {
-//         jugador1 = allFighters[nLuchador];
-
-//         let luchador1 = document.getElementById(nLuchador);
-
-//         document.getElementById(nLuchador).style.display = "none";
-
-//         luchador1.classList.add("fighterSelected");
-//         console.log(luchador1);
-//     } else if (jugador2 == "") {
-//         jugador2 = allFighters[nLuchador];
-
-//         let luchador2 = document.getElementById(nLuchador);
-
-//         document.getElementById(nLuchador).style.display = "none";
-
-//         luchador2.classList.add("fighterSelected");
-//         console.log(luchador2);
-
-//         setTimeout(()=>{
-//             cambiaPantalla(3);
-//         },2500);
-//     }
-
-// }
-
-// const selectFighter = (nLuchador) => {
-//     let luchadorDeseado = "luchador" + nLuchador;
-
-//     let allPersonajes = ["luchador0","luchador1","luchador2","luchador3","luchador4","luchador5","luchador6","luchador7"];
-    
-//     allPersonajes = allPersonajes.filter(valor => !luchadorDeseado.includes(valor));
-    
-//     document.getElementById("luchador" + nLuchador).style.display = "none";
-
-//     setTimeout(()=>{
-//         cambiaPantalla(3);
-//         },2500);
-    
-// }
-
 const selectFighter = (nLuchador) => {
     
     if(jugador1 == ""){
@@ -98,31 +55,18 @@ const displayGame = () => {
                                            
 }
 
-const transformFighter = (nLuchador) => {
-    let conversion = allPersonajes[nLuchador];
-    conversion.toString();
-    conversion.slice(7);
-    parseInt(conversion);
-
-    console.log(conversion);
-}
-
 let displayJugador1 = document.getElementById("pictureFighter1");
 let displayJugador2 = document.getElementById("pictureFighter2");
 let winner = document.getElementById("winner");
 
-// if (jugador1 == "fighter" + (nLuchador)) {
+let screen4 = document.getElementById("screen4");
 
-//     let allLuchadores = ["fighter1","fighter2","fighter3","fighter4","fighter5","fighter6","fighter7","fighter8"];
+document.body.addEventListener("keydown", (ev) => {
+            
+    if(screen4.style.display == "block"){
+        console.log("ataque");
+    }
 
-//     allLuchadores = allLuchadores.filter(valor => !luchadorDeseado.includes(valor));
-
-    
-    
-//     let luchadorDer = document.getElementById("personaje1");
-
-//     luchadorDer.classList.add("personajeSeleccionado1luchador" + (nLuchador));
-// }
+});
 
 // Algoritmo
-
